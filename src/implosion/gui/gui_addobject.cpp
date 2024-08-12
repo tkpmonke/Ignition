a3a345dfac80307b2594d0d9ed91933ff7342fd7
tkp <email@tkpmonke.com>
2024-08-11 22:30:43 -0400

    began working on website + added a little to obj loader

--- src/implosion/gui/gui_addobject.cpp
+++ src/implosion/gui/gui_addobject.cpp
@@ -19,62 +19,78 @@ using namespace Ignition::Rendering;
          Shader s = Shader(unlit_vertex, unlit_fragment, ShaderType::Unlit);                       \
          s.albedo = Texture();                                                                           \
          s.albedo.SetFlags(TextureFlags::Repeat | TextureFlags::Nearest);                                \
-         s.albedo.LoadData((unsigned char*)grid_texture, 8, 8, 3, "ignition_grid_texture");              \
+         s.albedo.LoadData((unsigned char*)grid_texture, 8, 8, 3, "ignition_grid_texture");              
 
 namespace Implosion {
    void GUI::AddObjectMenu()
    {
 
-
-      if (ImGui::MenuItem("Empty"))
+      if (ImGui::BeginMenu("Create Object"))
       {
-         Ignition::scene.CreateObject();
-      }
-      if (ImGui::MenuItem("Square"))
-      {
-         CREATE_SHADER(); 
-         MeshRenderer m = MeshRenderer(this->camera);
-         m.LoadModel(square_model, "square");
-         m.LoadShader(s);
-         std::shared_ptr<MeshRenderer> ptr = std::make_shared<MeshRenderer>(m);
-         Ignition::scene.GetObjects()->at(Ignition::scene.CreateObject()).AddModule(ptr);
-      }
-      if (ImGui::MenuItem("Cube"))
-      {
-         CREATE_SHADER(); 
-         MeshRenderer m = MeshRenderer(this->camera);
-         m.LoadModel(cube_model, "cube");
-         m.LoadShader(s);
-         std::shared_ptr<MeshRenderer> ptr = std::make_shared<MeshRenderer>(m);
-         Ignition::scene.GetObjects()->at(Ignition::scene.CreateObject()).AddModule(ptr);
-      }
-      if (ImGui::MenuItem("Performance Test"))
-      {
-         for (int i = 0; i < 1000; ++i)
+         if (ImGui::MenuItem("Empty"))
+         {
+            Ignition::scene.CreateObject();
+         }
+         if (ImGui::MenuItem("Square"))
+         {
+            CREATE_SHADER(); 
+            MeshRenderer m = MeshRenderer(this->camera);
+            m.LoadModel(square_model, "square");
+            m.LoadShader(s);
+            std::shared_ptr<MeshRenderer> ptr = std::make_shared<MeshRenderer>(m);
+            Ignition::scene.GetObjects()->at(Ignition::scene.CreateObject()).AddModule(ptr);
+         }
+         if (ImGui::MenuItem("Cube"))
          {
             CREATE_SHADER(); 
             MeshRenderer m = MeshRenderer(this->camera);
             m.LoadModel(cube_model, "cube");
             m.LoadShader(s);
             std::shared_ptr<MeshRenderer> ptr = std::make_shared<MeshRenderer>(m);
-            Ignition::Object* o = &Ignition::scene.GetObjects()->at(Ignition::scene.CreateObject());
-            int x = -50 + rand() % 100;
-            int y = -50 + rand() % 100;
-            int z = -50 + rand() % 100;
-
-            int rx = -180 + rand() % 360;
-            int ry = -180 + rand() % 360;
-            int rz = -180 + rand() % 360;
-            o->AddModule(ptr);
-            o->transform.position = Ignition::Vector3(x,y,z);
-            o->transform.rotation = Ignition::Vector3(rx,ry,rz);
-
+            Ignition::scene.GetObjects()->at(Ignition::scene.CreateObject()).AddModule(ptr);
          }
+         ImGui::EndMenu();
       }
       ImGui::Separator();
       if (ImGui::MenuItem("Clear All Objects"))
       {
          Ignition::scene.GetObjects()->clear();
       }
+#ifdef DEBUG
+      ImGui::Separator();
+      if (ImGui::BeginMenu("Debug")) { 
+         if (ImGui::MenuItem("Performance Test"))
+         {
+            for (int i = 0; i < 1000; ++i)
+            {
+               CREATE_SHADER(); 
+               MeshRenderer m = MeshRenderer(this->camera);
+               m.LoadModel(cube_model, "cube");
+               m.LoadShader(s);
+               std::shared_ptr<MeshRenderer> ptr = std::make_shared<MeshRenderer>(m);
+               Ignition::Object* o = &Ignition::scene.GetObjects()->at(Ignition::scene.CreateObject());
+               int x = -50 + rand() % 100;
+               int y = -50 + rand() % 100;
+               int z = -50 + rand() % 100;
+
+               int rx = -180 + rand() % 360;
+               int ry = -180 + rand() % 360;
+               int rz = -180 + rand() % 360;
+
+               float sx = .5f + rand() % 2;
+               float sy = .5f + rand() % 2;
+               float sz = .5f + rand() % 2;
+
+               o->AddModule(ptr);
+               o->transform.position = Ignition::Vector3(x,y,z);
+               o->transform.rotation = Ignition::Vector3(rx,ry,rz);
+               o->transform.scale =    Ignition::Vector3(sx,sy,sz);
+
+            }
+         }
+         ImGui::EndMenu();
+      }
+#endif
+      
    }
 }
